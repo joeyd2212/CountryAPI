d761dcfa3febf5d522d967fb3bced80a3f0fd8ad
DEE ALLEN <JOEYPBA09@GMAIL.COM>
2019-09-05 16:07:53 -0400

    fix bug

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,18 @@
+
 const mongoose = require("mongoose");
-// mongoose.connect("mongodb://localhost/countries_db", { useNewUrlParser: true });
-// mongoose.Promise = Promise;
 
 let mongoURI = "";
+
 if (process.env.NODE_ENV === "production") {
   mongoURI = process.env.DB_URL;
 } else {
   mongoURI = "mongodb://localhost/countries_db";
 }
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 module.exports = mongoose;
 
